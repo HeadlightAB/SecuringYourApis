d370fdb54470f1531a51538c46077b1038f93d12
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-08-17 13:20:44 +0200

    Minor cleansing

--- PublicMetarApi/Controllers/TafController.cs
+++ PublicMetarApi/Controllers/TafController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace PublicMetarApi.Controllers
 {
