87f2b993f839392e85aab8b9fad6ef1fc9a6862c
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-08-18 15:47:34 +0200

    Added IdentityServer to the IdSrv host

---
+++ IdSrv/IdStore.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer4.Models;
+
+namespace IdSrv
+{
+    public class IdStore
+    {
+        public static Client[] Clients { get; } = {
+            new Client
+            {
+                AllowedScopes = new[] {"public.api.read"},
+                ClientId = "weatherTV",
+                ClientSecrets = new[] {new Secret("secret.password.for.weathertv".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials
+            }
+        };
+
+        public static ApiResource[] ApiResources { get; } =
+        {
+            new ApiResource("public.api")
+            {
+                Scopes = new[]
+                {
+                    new Scope("public.api.read"),
+                    new Scope("public.api.write"),
+                }
+            }
+        };
+    }
+}
--- IdSrv/Startup.cs
+++ IdSrv/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,19 +8,22 @@ namespace IdSrv
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer(options => { options.IssuerUri = "https://idsv.weatherportal"; })
+                .AddDeveloperSigningCredential()
+                .AddInMemoryClients(IdStore.Clients)
+            .AddInMemoryApiResources(IdStore.ApiResources);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseIdentityServer();
 
             app.Run(async (context) =>
             {
